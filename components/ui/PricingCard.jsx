b76a826a6705f6a250739b64ad4a2e51e33d6bf9
Francisco Perez <fperezfrancisco4@gmail.com>
2025-03-15 11:21:41 -0700

    troubleshooting mobile border

--- components/ui/PricingCard.jsx
+++ components/ui/PricingCard.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const PricingCard = ({ serviceObject }) => {
   return (
-    <div className="w-[300px] h-[500px] rounded-3xl border-0 outline-4 sm:outline-0 sm:border-4 border-primary bg-white text-primary flex flex-col p-4 gap-4 items-center cursor-pointer hover:scale-110 transition-all ease-out duration-300">
+    <div
+      className="w-[300px] h-[500px] rounded-3xl border-0 outline-4 sm:outline-0 sm:border-4 border-primary bg-white text-primary flex flex-col p-4 gap-4 items-center cursor-pointer hover:scale-110 transition-all ease-out duration-300 border-fix"
+      style={{ WebkitAppearance: "none" }}
+    >
       <h3 className="text-xl font-semibold">{serviceObject.title}</h3>
       <div className="flex flex-col items-center my-8">
         <p className="text-xs md:text-sm">Starting at:</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    "border-4",
+    "border-gray-300",
+    "border-solid",
+    "outline",
+    "outline-4",
+    "outline-gray-300",
+  ],
   theme: {
     extend: {},
   },
